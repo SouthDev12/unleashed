66e6affe9bb111e29351b24affa48f9ebad933c4
Jason <jaysouth7@gmail.com>
2017-08-30 22:57:22 -0600

    Update URL config

--- organizer/views.py
+++ organizer/views.py
@@ -1,20 +1,25 @@
-from django.http.response import HttpResponse
+from django.http.response import (Http404, HttpResponse)
 from django.template import loader
+from django.shortcuts import (get_object_or_404, render)
 
-from .models import Tag
+from .models import Startup, Tag
 
 # Create your views here.
-def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template('organizer/tag_list.html')
-    context = {'tag_list': tag_list}
-    output = template.render(context)
-    return HttpResponse(output)
+def startup_list(request):
+    return render(request,'organizer/startup_list.html',
+        {'startup_list': Startup.objects.all()})
 
+def startup_detail(request, slug):
+    startup = get_object_or_404(Startup, slug__iexact=slug)
+    return render(request, 'organizer/startup_detail.html',
+        {'startup': startup})
+
+
+def tag_list(request):
+    return render(request,'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()})
 
 
 def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    template = loader.get_template('organizer/tag_detail.html')
-    context = ({'tag': tag})
-    return HttpResponse(template.render(context))
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render(request,'organizer/tag_detail.html',{'tag': tag})
