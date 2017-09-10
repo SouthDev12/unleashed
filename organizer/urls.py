a011e8589bd0830df1e49053363c18e31e9f3ae2
Jason <jaysouth7@gmail.com>
2017-09-09 23:38:59 -0600

    Template build outs

--- organizer/urls.py
+++ organizer/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^startup/(?P<slug>[\w\-]+)/$',startup_detail,name='organizer_startup_detail'),
     url(r'^tag/$', tag_list, name='organizer_tag_list'),
     url(r'^tag/(?P<slug>[\w\-]+)/$',tag_detail,name='organizer_tag_detail'),
+
 ]
